7cdc4d48c804921c760d9bfa793bb39717d5843b
Lucasalexisc <lucasalexisc@gmail.com>
2023-03-07 23:14:00 -0300

    commit final

--- scripts/toggleMenu.js
+++ scripts/toggleMenu.js
@@ -1,5 +1,6 @@
 let menuSign = document.querySelector('.menu-sign');
 let navbar = document.querySelector('.navbar');
+// let bod = document.querySelector('.container');
 
 menuSign.addEventListener('click', function() {
   menuSign.classList.toggle('isactive');
